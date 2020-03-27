18016b9c6201ae91f5124797adb0d933578d8c23
hailemaryam <hailemaryam123456!!!>
2020-03-27 22:46:15 +0300

    Custom Repository used in telegram db utility

--- src/main/java/et/com/delivereth/Telegram/DbUtility.java
+++ src/main/java/et/com/delivereth/Telegram/DbUtility.java
@@ -5,9 +5,7 @@ import et.com.delivereth.domain.Order;
 import et.com.delivereth.domain.OrderedFood;
 import et.com.delivereth.domain.TelegramUser;
 import et.com.delivereth.domain.enumeration.OrderStatus;
-import et.com.delivereth.repository.OrderRepository;
-import et.com.delivereth.repository.OrderedFoodRepository;
-import et.com.delivereth.repository.TelegramUserRepository;
+import et.com.delivereth.repository.*;
 import et.com.delivereth.service.TelegramUserService;
 import et.com.delivereth.service.dto.TelegramUserDTO;
 import org.springframework.stereotype.Service;
@@ -18,18 +16,18 @@ import java.util.Optional;
 @Service
 public class DbUtility {
     private final TelegramUserService telegramUserService;
-    private final TelegramUserRepository telegramUserRepository;
-    private final OrderRepository orderRepository;
+    private final CustomTelegramUserRepository customTelegramUserRepository;
+    private final CustomOrderRepository customOrderRepository;
     private final OrderedFoodRepository orderedFoodRepository;
     public DbUtility(
         OrderedFoodRepository orderedFoodRepository,
-        OrderRepository orderRepository,
+        CustomOrderRepository customOrderRepository,
         TelegramUserService telegramUserService,
-        TelegramUserRepository telegramUserRepository) {
-        this.orderRepository = orderRepository;
+        CustomTelegramUserRepository customTelegramUserRepository) {
+        this.customOrderRepository = customOrderRepository;
         this.orderedFoodRepository = orderedFoodRepository;
         this.telegramUserService = telegramUserService;
-        this.telegramUserRepository = telegramUserRepository;
+        this.customTelegramUserRepository = customTelegramUserRepository;
     }
     public void registerTelegramUser(Update update){
         TelegramUserDTO telegramUserDTO = new TelegramUserDTO();
@@ -47,16 +45,16 @@ public class DbUtility {
     }
     public void registerUserPhone(Update update) {
         Optional<TelegramUser> telegramUserByUserNameEquals =
-            this.telegramUserRepository
+            customTelegramUserRepository
                 .findTelegramUserByUserNameEquals(update.getMessage().getFrom().getUserName());
         TelegramUser telegramUser = telegramUserByUserNameEquals.get();
         telegramUser.setPhone(update.getMessage().getContact().getPhoneNumber());
         telegramUser.setConversationMetaData(ChatStepConstants.WAITING_FOR_ORDER_RESPONSE);
-        telegramUserRepository.save(telegramUser);
+        customTelegramUserRepository.save(telegramUser);
     }
     public TelegramUser getTelegramUser(Update update) {
         Optional<TelegramUser> telegramUserByUserNameEquals =
-            this.telegramUserRepository
+            customTelegramUserRepository
                 .findTelegramUserByUserNameEquals(update.getMessage().getFrom().getUserName());
         return telegramUserByUserNameEquals.isPresent()? telegramUserByUserNameEquals.get(): null;
     }
@@ -64,7 +62,7 @@ public class DbUtility {
         Order order = new Order();
         order.setTelegramUser(telegramUser);
         order.setOrderStatus(OrderStatus.STARTED);
-        return orderRepository.save(order);
+        return customOrderRepository.save(order);
     }
     public OrderedFood addItemToOrder(Order order, Food food, Integer quantity){
         OrderedFood orderedFood = new OrderedFood();
@@ -74,10 +72,10 @@ public class DbUtility {
         return orderedFoodRepository.save(orderedFood);
     }
     public Order updateOrder(Order order) {
-        return orderRepository.save(order);
+        return customOrderRepository.save(order);
     }
     public Order findOrder(TelegramUser telegramUser, OrderStatus orderStatus){
-        Optional<Order> byOrderStatusAndTelegramUser = orderRepository.findByOrderStatusAndTelegramUser(orderStatus, telegramUser);
+        Optional<Order> byOrderStatusAndTelegramUser = customOrderRepository.findByOrderStatusAndTelegramUser(orderStatus, telegramUser);
         return  byOrderStatusAndTelegramUser.isPresent()? byOrderStatusAndTelegramUser.get(): null;
     }
 }

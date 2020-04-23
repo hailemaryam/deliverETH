49fab2ea5e184c797e165a1118fd468a9899631a
hailemaryam <hailemaryam123456!!!>
2020-04-23 14:09:55 +0300

    static text modified.

--- src/main/java/et/com/delivereth/Telegram/Constants/StaticText.java
+++ src/main/java/et/com/delivereth/Telegram/Constants/StaticText.java
@@ -28,18 +28,18 @@ public class StaticText {
     public static final String orderSuccessfullyRegistered = "\uD83D\uDC68\u200D\uD83C\uDF73 Your order has been successfully sent.";
     public static final String orderSuccessfullyCanceled = "\uD83D\uDC68\u200D\uD83C\uDF73 Your order has been successfully canceled.";
 
-    public static final String chooseRestaurantListTitle = "<b>Choose restaurant from this list</b>";
+    public static final String chooseRestaurantListTitle = "<b>Choose store from this list</b>";
     public static final String yourOrderListTitle = "<b>Your Order List</b>";
-    public static final String noRestaurantListAround = "There are no restaurant list around you delivering at this time.";
-    public static final String wantToListMoreRestaurant = "Want to list more restaurant.";
+    public static final String noRestaurantListAround = "There are no stores list around you delivering at this time.";
+    public static final String wantToListMoreRestaurant = "Want to list more stores?";
 
     public static final String weNeedYourLocationText = "We need your location for order delivery. If location on your phone is not turned on please turn on and click share location to share your location.";
     public static final String weNeedYourLocationAgainText = "We can't process your order with out knowing your location. " +
         "Please click share location to share your location. If your location is not turned on, please turn on your location. ";
     public static final String locationOutOfAddisError = "❗️ Your selected location is not in Addis Ababa. If your location is not turned on, please turn on your location. ";
 
-    public static final String orderRejectedText = "<b>❗️ Your order is rejected by the restaurant</b>\n";
-    public static final String orderCancelBySystemText = "<b>❗️ Your order canceled since it didn't get response from service provider within 7 min. Please order again or order from other restaurant.</b>\n";
+    public static final String orderRejectedText = "<b>❗️ Your order is rejected by the store</b>\n";
+    public static final String orderCancelBySystemText = "<b>❗️ Your order canceled since it didn't get response from service provider within 7 min. Please order again or order from other store.</b>\n";
     public static final String orderCancelBySystemForRestaurantText = "<b>❗️ Order canceled since it didn't get response within 7 min. Please respond faster to avoid clients complain.</b>\n";
     public static final String orderStatuChanged = "<b>✅ Order Status Change</b>\n";
     public static final String noMoreOrder = "There are no more orders to load.";
@@ -50,18 +50,18 @@ public class StaticText {
 
     public static final String userHelp = "<b>\uD83D\uDCD6 Help</b>\n" +
         "DeliverEth is the first Ethiopian telegram bot to bring you fresh foods where ever you are.\n" +
-        "Order your favorite dish from your favorite restaurant and it will be delivered at your door steps.\n";
+        "Order your favorite dish from your favorite store and it will be delivered at your door steps.\n";
     public static final String userErrorCommand = "<b>❗️ Improper Command</b>\n" +
         "Your request could not be processed. you need to choose or write proper commands.";
     public static final String weNeedContact = "We need your contact for registration. Click share button to share your contact.";
     public static final String getWeNeedContactAgain = "We can't deliver our service with out your contact. Click share button to share your contact.";
-    public static final String noFoodMenuText = "Menu are not added in selected restaurant.";
+    public static final String noFoodMenuText = "Menu are not added in selected store.";
     public static final String successfullyTransportUserRegistration = "<b>\uD83D\uDC68\u200D\uD83C\uDF73 Your contact has been successfully registered. We will send order detail when it arrives.</b>\n";
 
     public static final String orderStatusSuccessfullyChanged = "\uD83D\uDC68\u200D\uD83C\uDF73 Your order status has been successfully changed.";
     public static final String alreadyCanceledByUser = "❗️ This order already canceled by user.";
     public static final String alreadyAccepted = "❗️ This order already accepted by other co-worker.";
     public static final String alreadyProccedByOtherUser = "❗️ This order already processed by other co-worker.";
-    public static final String notifyAccountLinkingNeed = "<b>\uD83D\uDC68\u200D\uD83C\uDF73 Your contact has been successfully registered. Contact deliverEth admin to link your account to your restaurant</b>\n";
-    public static final String notifyAccountLinkingNeedAgain = "<b>❗️ Before you continue the next step please wait deliverEth admin to link your account with your restaurant.</b>\n";
+    public static final String notifyAccountLinkingNeed = "<b>\uD83D\uDC68\u200D\uD83C\uDF73 Your contact has been successfully registered. Contact deliverEth admin to link your account to your store</b>\n";
+    public static final String notifyAccountLinkingNeedAgain = "<b>❗️ Before you continue the next step please wait deliverEth admin to link your account with your store.</b>\n";
 }

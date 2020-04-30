9699d8f7b5bfe8a894bff27db12a93a77a496797
hailemaryam <hailemaryam123456!!!>
2020-04-30 18:26:48 +0300

    bot name changed.

--- src/main/java/et/com/delivereth/Telegram/Constants/TelegramBotConstantCredentials.java
+++ src/main/java/et/com/delivereth/Telegram/Constants/TelegramBotConstantCredentials.java
@@ -5,14 +5,14 @@ public class TelegramBotConstantCredentials {
 
 //    public static final String BOT_TOKEN = "860857039:AAEo3n3qOUWDwRULRHXVUcvtVToOd2nSl8Q"; //Test
 
-    public static String BOT_TOKEN = "767968929:AAEnlgcBf1a4kcnVbhhgywkL8-sxipSphrs";
+    public static String BOT_TOKEN = "1179200079:AAGmt6tN7i00ekPJBXV63he1NrAOlrR-XlQ";
 
 //    public static final String BOT_TOKEN_RESTAURANT = "877442009:AAFyGH6C7Asy1WV_4_tOFPjAivhTpSNK6oY"; // test
 
-    public static final String BOT_TOKEN_RESTAURANT = "1165377856:AAEwJSCpRnodoTR-0Qbp7egMhtKs314BvKM";
+    public static final String BOT_TOKEN_RESTAURANT = "1271496542:AAEfZN0UcTyzgnxNcV8cNlMMRqQoBeBGwxk";
 
 //    public static final String BOT_TOKEN_TRANSPORT = "833798956:AAEbnR7uhS7V44xaci7kPesIyh91pI6PAq8"; // test
 
-    public static final String BOT_TOKEN_TRANSPORT = "1075292594:AAEArNhuvMP-WXnPOpj8ChiXXdiIwAozJH4";
+    public static final String BOT_TOKEN_TRANSPORT = "1261290375:AAH2k2fmech3q7AnDyjiOdXMY5VDz3fGTbY";
 
 }
